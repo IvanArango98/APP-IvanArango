177b1b968006b0725c3f67bed973f821f352df6e
IvanArango98 <iaarango@correo.url.edu.gt>
2025-04-30 15:12:22 -0600

    se actualiza modal de compra a responsive y restablecimiento de contraseña

--- frontend/src/Components/Login/ForgotPasswordModal.js
+++ frontend/src/Components/Login/ForgotPasswordModal.js
@@ -16,12 +16,14 @@ const modalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '90%', 
+  maxWidth: 400,
   bgcolor: 'background.paper',
   boxShadow: 24,
   borderRadius: 2,
   p: 4
 };
+
 
 const ForgotPasswordModal = ({ open, onClose }) => {
   const [email, setEmail] = useState('');
